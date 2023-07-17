e0110dddc2015b6fc759767f267b3d6fd03801b9
Jaga <jagadheesh6@gmail.com>
2023-07-17 19:11:35 +0530

    feat - update features default dto validation

--- src/common/dto/FeatureDefault.dto.ts
+++ src/common/dto/FeatureDefault.dto.ts
@@ -10,12 +10,12 @@ export class FeatureDefault {
 /**Features which has access to Message templates */
 export class FeatDefaultWithTemplates extends FeatureDefault {
   @IsNotEmpty()
-  @ValidateIf((o) => o !== null)
+  @ValidateIf((o) => o.channelId !== null)
   @IsString()
   public channelId: string;
 
   @IsNotEmpty()
-  @ValidateIf((o) => o !== null)
+  @ValidateIf((o) => o.templateId !== null)
   @IsString()
   public templateId: string;
 }
