6d4eaddc7a9b4cb7bcf0c6fd524b746bdd0e1d3a
Jaga <jagadheesh6@gmail.com>
2023-07-23 21:32:58 +0530

    feat - cache lang filter config during update

--- src/modules/language/language.service.ts
+++ src/modules/language/language.service.ts
@@ -1,15 +1,19 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Redis } from 'ioredis';
+import { Model } from 'mongoose';
 import { FeaturesEnum } from 'src/common/enum/features.enum';
 import { PROVIDER_TYPES } from 'src/core/provider.types';
 import { FeaturesRepo } from 'src/modules/features/repository/features.repo';
 import { LanguageDto } from 'src/modules/language/dto/language.dto';
+import { DataLibs } from 'src/modules/language/models/data_libs.model';
 
 @Injectable()
 export class LanguageService {
   constructor(
     @Inject(FeaturesRepo) private readonly featuresRepo: FeaturesRepo,
     @Inject(PROVIDER_TYPES.RedisClient) private readonly redisClient: Redis,
+    @InjectModel('data_libs') private readonly dataLibsModel: Model<DataLibs>,
   ) {}
 
   async viewLanguage(guildId: string): Promise<LanguageDto> {
@@ -40,5 +44,32 @@ export class LanguageService {
         languageUpdateDto.languageFilter.isActive.toString(),
       ),
     ]);
+
+    //Cache Language Filter config
+    languageUpdateDto.languageFilter.languageFilterConfig.map(async (e) => {
+      this.redisClient.set(
+        `guild-${guildId}:feature:languageFilterConfig`,
+        JSON.stringify(languageUpdateDto.languageFilter.languageFilterConfig),
+        'EX',
+        300,
+      );
+
+      if (e.isActive) {
+        const dataLib = await this.dataLibsModel.findOne({
+          _id: e.dataLibId,
+          guildId,
+        });
+
+        //Cache Data Libs
+        if (dataLib) {
+          this.redisClient.set(
+            `guild-${guildId}:dataLib-${e.dataLibId}`,
+            JSON.stringify(dataLib.data),
+            'EX',
+            300,
+          );
+        }
+      }
+    });
   }
 }
