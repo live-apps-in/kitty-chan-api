e1309b5f173a874cb08f872dbe1044b47ee8afef
Jaga <jagadheesh6@gmail.com>
2023-12-23 17:40:13 +0530

    feat - redis retry strategy

--- src/providers/redis.provider.ts
+++ src/providers/redis.provider.ts
@@ -7,10 +7,25 @@ const redisClient = new Redis({
   db: 1,
   password: process.env.REDIS_PASS,
   port: 6379,
-  retryStrategy: () => {
-    return null;
-  },
+  retryStrategy: redisRetryStrategy(10, 5000),
 });
+
+function redisRetryStrategy(maxRetries: number, interval: number) {
+  let retryCount = 0;
+
+  return () => {
+    if (retryCount < maxRetries) {
+      retryCount += 1;
+      console.log(
+        `🟡 kitty chan Domain Redis - Retry attempt ${retryCount} in ${
+          interval / 1000
+        } seconds...`,
+      );
+      return interval;
+    }
+    return null;
+  };
+}
 
 export const RedisProvider: Provider = {
   provide: PROVIDER_TYPES.RedisClient,
