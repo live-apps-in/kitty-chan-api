44288707cadd4a96dce1665ed5ed3983ea098e91
Jaga <jagadheesh6@gmail.com>
2024-02-03 22:09:18 +0530

    feat - update autosail constraints type

--- src/modules/auto-sail/enum/auto-sail-constraints-type.enum.ts
+++ src/modules/auto-sail/enum/auto-sail-constraints-type.enum.ts
@@ -1,6 +1,7 @@
 export enum AutoSailConstraintsType {
-  MESSAGE = 'message',
-  CREATED_AT = 'createdAt',
+  PLAIN_MESSAGE = 'plainMessage',
   USER_ID = 'userId',
   CHANNEL_ID = 'channelId',
+  CREATED_AT = 'createdAt',
+  CURRENT_DATE = 'currentDate',
 }
